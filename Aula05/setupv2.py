34f548ad0e52cc0457722f2324a140bd64128bb4
edwillie <edwillie@gmail.com>
2026-05-20 20:32:10 -0300

    intervalo

--- Aula05/setupv2.py
+++ Aula05/setupv2.py
@@ -15,6 +15,7 @@ from sklearn.metrics import accuracy_score, classification_report
 
 # ── CONFIGURAÇÕES ─────────────────────────────────────────────────────────────
 DIR_BASE      = Path(__file__).resolve().parent
+# DIR_BASE      = DIR_BASE / "Aula05"
 DIR_DADOS     = DIR_BASE / "dados_usuarios"
 MODELO_PATH   = DIR_BASE / "modelo_acesso.pkl"
 IMG_SIZE      = (64, 64)   # tamanho de cada rosto capturado
@@ -29,7 +30,27 @@ detectorRosto = cv2.CascadeClassifier(HAAR_PATH)
 DIR_DADOS.mkdir(exist_ok=True)
 
 # ── 1. CADASTRO DE USUÁRIOS ───────────────────────────────────────────────────
+def criarUsuario():
+    nomeUsuario = input("Digite o Nome do Usuario: ")
+    dirUsuario = f"{DIR_DADOS}/{nomeUsuario}"
+    dirUsuario.mkdir(exist_ok=True)
 
+    cap = cv2.VideoCapture(0)
+    contagem = 0
+
+    print(f"=== Cadastrando {nomeUsuario} ===")
+    print("Pressione 'q' para sair.")
+
+    while contagem < N_FOTOS:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        rostos = detectorRosto.detectMultiScale(cinza, scaleFactor=1.1, minNeighbors=5, minSize=(80,80))
+        
 
 # ── 2. CARREGAMENTO DAS FOTOS CADASTRADAS ─────────────────────────────────────
 
