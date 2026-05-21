7940117f20132a4d6f011c1c876ad5967cec1767
edwillie <edwillie@gmail.com>
2026-05-20 21:50:34 -0300

    final de aula

--- Aula05/setupv2.py
+++ Aula05/setupv2.py
@@ -32,7 +32,7 @@ DIR_DADOS.mkdir(exist_ok=True)
 # ── 1. CADASTRO DE USUÁRIOS ───────────────────────────────────────────────────
 def criarUsuario():
     nomeUsuario = input("Digite o Nome do Usuario: ")
-    dirUsuario = f"{DIR_DADOS}/{nomeUsuario}"
+    dirUsuario = DIR_DADOS / nomeUsuario
     dirUsuario.mkdir(exist_ok=True)
 
     cap = cv2.VideoCapture(0)
@@ -50,10 +50,51 @@ def criarUsuario():
         cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         rostos = detectorRosto.detectMultiScale(cinza, scaleFactor=1.1, minNeighbors=5, minSize=(80,80))
-        
+
+        for (x, y, w, h) in rostos:
+            rosto = cinza[y:y+h, x:x+w]
+            rosto = cv2.resize(rosto, IMG_SIZE)
+            caminhoFoto = dirUsuario / f"foto_{contagem:03d}.jpg"
+            _,buf = cv2.imencode(".jpg",rosto)
+            buf.tofile(str(caminhoFoto))
+            contagem += 1
+
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+            cv2.putText(frame, f"{nomeUsuario}:{contagem}/{N_FOTOS}", (x,y -10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
+
+            cv2.imshow("Cadastro de Usuario", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    print("Fotos Adquiridas!")
+
+    return contagem
 
 # ── 2. CARREGAMENTO DAS FOTOS CADASTRADAS ─────────────────────────────────────
 
 # ── 3. TREINAMENTO: PCA + REGRESSÃO LOGÍSTICA ────────────────────────────────
 
 # ── MENU PRINCIPAL ────────────────────────────────────────────────────────────
+if __name__=="__main__":
+    while True:
+        print("1. Cadastrar Usuario")
+        print("2. Treinar o Modelo")
+        print("3. Sair")
+
+        opcao = int(input("\n Selecione sua opção: "))
+
+        if opcao == 1:
+            cont = criarUsuario()
+        elif opcao == 2:
+            pass
+        else:
+            break
+
+
+
+
+
